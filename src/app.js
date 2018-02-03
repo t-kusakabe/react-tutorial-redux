15a1040525b2b8b49959de4b6ed5d98d775a2c38
t-kusakabe <ttmac0704@gmail.com>
2018-02-03 23:33:32 +0900

    Create react redux

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
-import expect from 'expect'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
 
+// reducer
 const counter = (state = 0, action) => {
   switch(action.type) {
     case 'INCREMENT':
@@ -11,24 +14,31 @@ const counter = (state = 0, action) => {
   }
 }
 
-expect(
-  counter(0, {type: 'INCREMENT'})
-).toEqual(1)
+const Counter = ({value, onIncrement, onDecrement}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+)
 
-expect(
-  counter(1, {type: 'INCREMENT'})
-).toEqual(2)
+const store = createStore(counter)
 
-expect(
-  counter(2, {type: 'DECREMENT'})
-).toEqual(1)
+const render = () => {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({
+        type: 'INCREMENT'
+      }) }
+      onDecrement={() => store.dispatch({
+        type: 'DECREMENT'
+      })}
+    />,
+    document.getElementById('root')
+  )
+}
 
-expect(
-  counter(1, {type: 'UNKNOWN'})
-).toEqual(1)
+store.subscribe(render)
 
-expect(
-  counter(undefined, {})
-).toEqual(0)
-
-console.log('test passed!')
+render()
