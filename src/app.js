7113966610ea6d6ace2e4fac7ee49e07761b707a
t-kusakabe <ttmac0704@gmail.com>
2018-02-03 18:14:47 +0900

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import expect from 'expect'
+
+const counter = (state, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+  }
+}
+
+expect(
+  counter(0, {type: 'INCREMENT'})
+).toEqual(1)
+
+expect(
+  counter(1, {type: 'INCREMENT'})
+).toEqual(2)
+
+expect(
+  counter(2, {type: 'DECREMENT'})
+).toEqual(1)
+
+console.log('test passed!')
